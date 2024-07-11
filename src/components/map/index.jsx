266993a8ae87cea8cc99b936a3e5c8cd7eb158fc
Jesse Guerrero <jesseguerrero.atx@gmail.com>
2024-07-10 19:53:23 -0600

    added swr package for api fetching, added ability to generate dynamic markers on map based on api data, added popups based on api data

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,19 +1,43 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then(response => response.json());
 
 const map = (props) => {
 
+    const url = "https://menus-api.vercel.app/";
+    const { data, error } = useSWR(url, { fetcher });
+    const restaurantsData = data && !error ? data["bbqs"].slice(0, 20) : [];
+
     return (
 
-        <MapContainer className="h-[300px] w-5/6" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer className="h-[400px] w-5/6 m-3 p-2" center={[30.239260, -97.709444]} zoom={10} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+
+            {restaurantsData.map(r => (
+
+                <Marker key={r.id} position={[r.latitude, r.longitude]} >
+                    <Popup>
+                        <div className='flex flex-col'>
+                            <div className='text-lg font-bold m-2'>{r.name}</div>
+                            <div className='flex'>
+                                <div className='m-2'>
+                                    <img className="w-48" src={r.img} alt="" />
+                                </div>
+                                <div className='flex flex-col m-2'>
+
+                                    <div className='mb-5'>Known for: {r.dsc}</div>
+                                    <div className='flex justify-end'>{r.rate} stars</div>
+                                </div>
+                            </div>
+                        </div>
+                    </Popup>
+                </Marker>
+            ))}
+
         </MapContainer>
     )
 }
