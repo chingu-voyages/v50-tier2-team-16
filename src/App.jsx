0cb65638435127d7fdc80ed63b226e7c16a683c8
Matthew Neie <matneie@gmail.com>
2024-07-22 15:14:20 -0700

    add order list

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import './App.css'
 import Map from "./components/organisms/map";
 import UserDashboard from "./components/molecules/userDashboard";
 import { UserProvider } from "./contexts/UserContext";
 import Menu from './components/MenuAPI'
+import OrderList from './components/Order'
 
 
 function App() {
 
+  const [ order, setOrder ] = useState([])
+
   return (
     <>
-      <UserProvider>
-        <UserDashboard />
-        <Map />
-        <Menu />
-      </UserProvider>
+      <div className="app-container">
+        <div className="app-left">
+          <UserProvider>
+            <UserDashboard />
+            <Map />
+            <Menu order={order} setOrder={setOrder}/>
+          </UserProvider>
+        </div>
+          <OrderList order={order} setOrder={setOrder}/>
+      </div>
     </>
   )
 }
