5089a2ddb3041674dfeaa25866c5bfc697a154b0
yusuken1121 <yusuken1121@gmail.com>
2024-07-25 21:50:13 -0700

    Added a hamburger menu from React Icon

---
+++ src/components/atoms/header/Hamburger.jsx
@@ -0,0 +1,11 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Hamburger = () => {
+  return (
+    <div>
+      <GiHamburgerMenu className="h-5 w-5" />
+    </div>
+  );
+};
+
+export default Hamburger;
