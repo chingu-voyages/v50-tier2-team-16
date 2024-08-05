315f570f9e7af8ff32f4bbed08a8eca2a880f68d
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-08-05 11:30:40 -0700

    resolved conflicts in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,23 +14,7 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-       <UserProvider>
-       <FilterProvider>
-      <div className="app-container">
-        <div className="app-left">
-         
-              <Header />
-              <div className="pt-44 sm:pt-28">
-                <UserDashboard />
-                <Filter />
-                <Map />
-                <MenuResults order={order} setOrder={setOrder} />
-                <Footer />
-              </div>
-            
-         
-=======
+
       <div className="app-container">
         <div className="app-left">
           <UserProvider>
@@ -43,16 +27,10 @@ function App() {
               <Footer />
             </FilterProvider>
           </UserProvider>
->>>>>>> 89efe7fb895619a5c99cefe181c69fc17cdc6eaa
         </div>
         <OrderList />
       </div>
-<<<<<<< HEAD
-     
-      </FilterProvider>
-      </UserProvider>
-=======
->>>>>>> 89efe7fb895619a5c99cefe181c69fc17cdc6eaa
+
     </>
   );
 }
