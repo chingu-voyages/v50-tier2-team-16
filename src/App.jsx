880d489fb88912976aa6b062bf2ecd5c43d5ae4a
Jesse Guerrero <jesseguerrero.atx@gmail.com>
2024-08-05 15:07:33 -0600

    got rid of userDashboard on the homepage now that user details are in Order menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import Map from "./components/organisms/map";
-import UserDashboard from "./components/organisms/userDashboard";
 import { UserProvider } from "./contexts/UserContext";
 import OrderList from "./components/Order";
 import Filter from "./components/organisms/Filter";
@@ -18,14 +17,11 @@ function App() {
         <FilterProvider>
           <div className="app-container">
             <div className="app-left">
-
               <Header />
-              <UserDashboard />
               <Filter />
               <Map />
               <MenuResults />
               <Footer />
-
             </div>
             <OrderList />
           </div>
