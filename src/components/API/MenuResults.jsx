91dff26802f6cb4c088323c6f9a40d6b8cdc6693
Jesse Guerrero <jesseguerrero.atx@gmail.com>
2024-08-04 20:09:29 -0600

    Fixed the brackets so app starts again

--- src/components/API/MenuResults.jsx
+++ src/components/API/MenuResults.jsx
@@ -4,89 +4,91 @@ import "./MenuResults.css";
 import { useLocation } from "../../contexts/FilterContext";
 
 export function MenuResults() {
-  const [data, setData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const { foodtype, state, city } = useLocation();
-  let result = []
+    const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const { foodtype, state, city } = useLocation();
+    let result = []
 
-  
 
-  React.useEffect(() => {
-    async function getResults() {
-      const menuData = await getMenuAPI();
-      setData(menuData);
-    }
-    getResults();
-  }, []);
 
-  React.useEffect(() => {
-    // console.log("city", city);
-
-    function FilterData() {
-      setData(ByCountry())
-      // setData(ByFoodtype())
-      // return data
-    }
-    setFilteredData(FilterData())
-
-  }, [foodtype, state, city]);
-
-  function ByCountry() {
-   
-    if (data) {
-
-      result = [...Object.values(data)].flat().filter((item) => {
-      
-        item.country === `${city}, ${state}` 
-          return ;
-    
-          const splitCountry = item.country.split(", ");
-          console.log("splitCountry", splitCountry);
-         
-          let test =  splitCountry.every(loc => {
-            loc[0] === city || loc[1] === state 
-            console.log('state or city registered')
-     
-            });  
+    React.useEffect(() => {
+        async function getResults() {
+            const menuData = await getMenuAPI();
+            setData(menuData);
         }
-      )
-  }
+        getResults();
+    }, []);
 
-  // function ByFoodtype() {
-    
-  //   if (data) {
-  //     result = [...Object.values(data)].flat().filter((item) => {
-  //       if (item[foodtype] === foodtype) {
-  //         return item[foodtype];
-  //       }
-  //       return result;
-  //     });
-  //   }
-  // }
-  // console.log('city', city)
-  console.log("data", data);
-  console.log("filteredData", filteredData);
+    React.useEffect(() => {
+        // console.log("city", city);
 
-  return (
-    <div className="grid grid-cols-4 gap-4">
-      <h1>Results:</h1>
-      {!filteredData?.length && <h1>Filter criteria cannot find a match.</h1>}
+        function FilterData() {
+            setData(ByCountry())
+            // setData(ByFoodtype())
+            // return data
+        }
+        setFilteredData(FilterData())
 
-      {filteredData && filteredData?.map((item, index) => (
-        <div key={index} className="data-map-div ">
-          <h1>{item.name}</h1>
-          <img
-            className="h-auto max-w-lg rounded-lg"
-            src={item.img}
-            height="250px"
-            alt={item.name}
-          ></img>
-          <h2>{item.country}</h2>
-          <h2>{item.dsc}</h2>
+    }, [foodtype, state, city]);
+
+    function ByCountry() {
+
+        if (data) {
+
+            result = [...Object.values(data)].flat().filter((item) => {
+
+                item.country === `${ city }, ${ state }`
+
+
+                const splitCountry = item.country.split(", ");
+                console.log("splitCountry", splitCountry);
+
+                let test = splitCountry.every(loc => {
+                    loc[0] === city || loc[1] === state
+                    console.log('state or city registered')
+
+                });
+            }
+            )
+        }
+    }
+
+    // function ByFoodtype() {
+
+    //   if (data) {
+    //     result = [...Object.values(data)].flat().filter((item) => {
+    //       if (item[foodtype] === foodtype) {
+    //         return item[foodtype];
+    //       }
+    //       return result;
+    //     });
+    //   }
+    // }
+    // console.log('city', city)
+    console.log("data", data);
+    console.log("filteredData", filteredData);
+
+    return (
+        <div className="grid grid-cols-4 gap-4">
+            <h1>Results:</h1>
+            {!filteredData?.length && <h1>Filter criteria cannot find a match.</h1>}
+
+            {filteredData && filteredData?.map((item, index) => (
+                <div key={index} className="data-map-div ">
+                    <h1>{item.name}</h1>
+                    <img
+                        className="h-auto max-w-lg rounded-lg"
+                        src={item.img}
+                        height="250px"
+                        alt={item.name}
+                    ></img>
+                    <h2>{item.country}</h2>
+                    <h2>{item.dsc}</h2>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
+
 }
 
 export default MenuResults;
