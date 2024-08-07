54aa54c6b2db590dc22353807a54a507ffe24721
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-08-07 11:40:35 -0700

    fix margin top on filter component to avoid overlap with header

--- src/components/organisms/Filter.jsx
+++ src/components/organisms/Filter.jsx
@@ -4,18 +4,18 @@ import FoodDropdown from '../molecules/filter/FoodDropdown'
 
 export default function Filter(){
     return(
-        <div className='m-2 w-5/6 border-black black border-2 py-2 px-2 grid grid-cols-3 gap-1;'>
-            <div className='firstFilter'>
+        <div className='mt-28 m-2 w-5/6 border-black black border-2 py-2 px-2 grid grid-cols-3 gap-1;'>
+            <div className='firstFilter mt-4 m-0'>
                 Filter by food category:
                 <FoodDropdown />
             </div>
             
-            <div className='secondFilter'>
+            <div className='secondFilter mt-4'>
                 <h1>Choose your state:</h1>
                 <StateDropdown />
             </div>
         
-            <div>
+            <div className='thirdFilter mt-4'>
                 <h1>Want to check if we deliver in your city?:</h1>
                 <CitySearch />
             </div>
